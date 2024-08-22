a6886d597fc23ca7d3f2e6c30b035cc70a959c1b
AlvaroCastro1 <jesus.27castrop@gmail.com>
2024-08-21 18:52:53 -0600

    PrimerP: transicion cuadrado a circulo

--- clase_Manim/PrimerP.py
+++ clase_Manim/PrimerP.py
@@ -1,9 +1,13 @@
 from manim import *
 
 class FirstScene(Scene):
-
     def construct(self):
+        # se crea un cuadrado
         sq = Square()
+        # se crea un circulo con opacidad completa
         circ = Circle().set_fill(opacity=1)
+        # se transforma el cuadrado en el circulo con una animacion
         self.play(Transform(sq, circ))
+
+        # y se espera hasta que la animacion este completa
         self.wait()
