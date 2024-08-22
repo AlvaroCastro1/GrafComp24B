6bd9f9ff0f69a662bea23f49a26a880a5d529ffc
AlvaroCastro1 <jesus.27castrop@gmail.com>
2024-08-21 19:00:45 -0600

    DosM: se muestra funcion

--- clase_Manim/DosM.py
+++ clase_Manim/DosM.py
@@ -3,5 +3,8 @@ from manim import *
 
 class SecondScene(Scene):
     def construct(self):
+        # se crea una expresion con MathTex
         text = MathTex("x^2")
+
+        # y se añade a la escena para que aparezca
         self.add(text)
